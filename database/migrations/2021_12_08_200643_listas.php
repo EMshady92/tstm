b3e94a408ff7f8f5a41830777f8b9de0574f78fe
alex316123 <alex316123@gmail.com>
2021-12-08 14:30:47 -0600

    MIGRACION_LISTAS

---
+++ database/migrations/2021_12_08_200643_listas.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Listas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listas', function (Blueprint $table) {
+            $table->id();
+            $table->string('TIPO');
+            $table->string('NOMBRE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listas');
+    }
+}
